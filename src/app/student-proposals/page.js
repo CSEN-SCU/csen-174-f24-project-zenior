b26c9e56f34652ce207cec580fd7bcb334856f9a
Mia <mia.lassiter14@gmail.com>
2024-10-13 20:18:20 -0700

    beginning of proposals page
    markdown files for reading data from the file system. And pre rendering

--- src/app/student-proposals/page.js
+++ src/app/student-proposals/page.js
@@ -1,3 +1,14 @@
+import { getSortedPostsData } from '/lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
 export default function Student(){
 	return <h1>Student Proposals Page</h1>
 }
