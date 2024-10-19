411a743bb2541ff8477c8f6e5f1446bc2fd7c4ec
Mia <mia.lassiter14@gmail.com>
2024-10-19 14:11:23 -0700

    install form
    install shadcn component

--- src/app/proposal-form/page.jsx
+++ src/app/proposal-form/page.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Faculty() {
-  return <h1>Proposal Form </h1>;
+  return(
+    <section>
+        <h1>Project Proposal Form</h1>
+        <p>This form creates a SD project proposal, which will be shown on your profile. You may edit any of the fields, at any time</p>
+    </section>
+  );
 }
