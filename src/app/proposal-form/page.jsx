4c7563c9b78cb519ccfd39e9887ed734de3ea50d
Mia <mia.lassiter14@gmail.com>
2024-10-20 16:36:30 -0700

    Add styling to project proposal form
    Used tailwind css to add styling to the proposal form.
    
    Problems still:
    
    -need to send data to db upon clicking the submit button
    
    - use ajax to populate advisors' names (?) and ensure that users cannot enter  names of people not in the db
    
    ^same goes with project team members, potentially
    
    - i hardcoded a department to be checked already, but this should come from the database. The student or professor's department should be checked automatically based on what department they are in. And it should not be able to be unchecked.

--- src/app/proposal-form/page.jsx
+++ src/app/proposal-form/page.jsx
@@ -5,8 +5,6 @@ import ProposalForm from "../../components/ProjectProposalForm/Proposal";
 export default function Faculty() {
   return(
     <section>
-        <h1>Project Proposal Form</h1>
-        <p>This form creates a SD project proposal, which will be shown on your profile. You may edit any of the fields, at any time</p>
         <ProposalForm/>
     </section>
   );
