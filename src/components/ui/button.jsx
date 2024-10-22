a4124a44e146a5bb9aeca0baf368493dd3c4698f
vaniaggarwal <52869093+vaniaggarwal@users.noreply.github.com>
2024-10-21 18:55:22 -0700

    updated My-Team
    Created the box with the student overview for project
    Missing: routing implementation to the different pages and backend information stored in the database

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -5,7 +5,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-normal rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 min-w-0",
   {
     variants: {
       variant: {
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        custom: "bg-[#9e1b32] text-white border-none rounded-md cursor-pointer", // added custom 
+        custom: "bg-[#9e1b32] text-white px-4 py-2 rounded-lg hover:bg-red-700 focus:ring-red-500 text-center", // added custom 
       },
       size: {
         default: "h-10 px-4 py-2",
