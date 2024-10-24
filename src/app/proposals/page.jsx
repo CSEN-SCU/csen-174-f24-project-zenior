a90ff6a281ea400403cea489a930cfe4a6abc8f9
Mia <mia.lassiter14@gmail.com>
2024-10-24 14:54:41 -0700

    basic layout
    pagination basic structure

--- src/app/proposals/page.jsx
+++ src/app/proposals/page.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { projects } from "@/lib/server/actions";
 import Projects from "@/components/Projects";
+import PaginationControls from '@/components/PaginationControls'
+
+const data = [
+  'proposal 1',
+  'proposal 2',
+  'proposal 3',
+  'proposal 4',
+  'proposal 5',
+  'proposal 6',
+]
 
 export default async function Student() {
   const allProjects = await projects.get();
@@ -8,6 +18,22 @@ export default async function Student() {
   if (!projects || !projects.length) {
     return <h1>No projects found</h1>;
   } else {
-    return <Projects projects={allProjects} />;
+    //return <Projects projects={allProjects} />;
+    const start = (Number(page) - 1) * Number(per_page);
+    const end = start + Number(per_page);
+    const proposals = data.slice(start, end);
+
+    return(
+      <div className = 'flex flex-col gap-2 items-center'>
+        {proposals.map((proposal) => (
+          <p key={proposal}>{proposal}</p>
+        ))}
+
+        <PaginationControls 
+          hasNextPage = {end < data.length}
+          hasPrevPage = {start > 0}
+        />
+      </div>
+    )
   }
 }
