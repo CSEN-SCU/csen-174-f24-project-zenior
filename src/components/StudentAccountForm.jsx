9f3d0c1251c23c632949b3660f98a1aa47dfb13e
vaniaggarwal <52869093+vaniaggarwal@users.noreply.github.com>
2024-10-31 10:10:09 -0700

    Created Faculty Profile Page
    created the form and put it under faculty
    also created a page.jsx for the profile navigation
    need to make Department portion a dropdown

--- src/components/StudentAccountForm.jsx
+++ src/components/StudentAccountForm.jsx
@@ -10,10 +10,10 @@ import { usePathname } from "next/navigation";
 
 const StudentAccountForm = ({ user, userUpdate, hideInstruction }) => {
   const [formData, setFormData] = useState({
-    name: user.student.firstName + " " + user.student.lastName,
-    major: user.student.major,
-    minor: user.student.minor,
-    skills: user.student.skills.map((skill) => skill.skill.name),
+    name: user.student ? `${user.student.firstName} ${user.student.lastName}` : "",
+    major: user.student?.major || "",
+    minor: user.student?.minor || "",
+    skills: user.student?.skills?.map((skill) => skill.skill.name) || [],
   });
 
   // track input for skills
