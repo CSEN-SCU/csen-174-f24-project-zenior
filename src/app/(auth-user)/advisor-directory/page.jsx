a0f3324f325e4ee280f9e7b7ace26443da0bf8ca
Mia <mia.lassiter14@gmail.com>
2024-11-04 15:51:11 -0800

    create advisor directory template
    mostly repeat code from proposals list view and sidebar

--- src/app/(auth-user)/advisor-directory/page.jsx
+++ src/app/(auth-user)/advisor-directory/page.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { faculty } from "@/lib/server/actions";
-import Link from "next/link";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { DirectorySidebar } from "@/components/sidebar/directory-sidebar";
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default async function Page() {
   const allFaculty = await faculty.get();
@@ -8,6 +16,8 @@ export default async function Page() {
   if (!allFaculty || !allFaculty.length) {
     return <h1>No faculty found</h1>;
   }
+
+  /*
 
   return (
     <ul>
@@ -22,4 +32,85 @@ export default async function Page() {
       ))}
     </ul>
   );
+} */
+
+function createData(firstName, lastName, department) {
+  return {firstName, lastName, department};
+}
+
+/* Will need to pull data from the db */
+const rows = [
+  createData(
+    "Michael",
+    "Abbott",
+    "Mechanical Engineering"
+  ),
+  createData(
+    "Margareta",
+    "Ackerman",
+    "Computer Science and Engineering"
+  ),
+  createData(
+    "Ahmed",
+    "Amer",
+    "Computer Science and Engineering"
+  ),
+  createData(
+    "Aria",
+    "Amirbahman",
+    "Civil, Environmental and Sustainable Engineering"
+  ),
+  createData(
+    "Ismail",
+    "Araci",
+    "Bioengineering"
+  ),
+  createData(
+    "Prashanth",
+    "Asuri",
+    "Bioengineering"
+  ),
+  createData(
+    "Darren",
+    "Atkinson",
+    "Computer Science and Engineering"
+  ),
+]
+
+  return(
+    <div className="px-8 m-9">
+
+      <div className="flex flex-row">
+      <div>
+        <SidebarProvider>
+          <DirectorySidebar/>
+        </SidebarProvider> 
+      </div>
+
+        <div>
+          <h1 className="font-black text-3xl pb-6">Project Proposals</h1>
+        <TableContainer component = {Paper}>
+          <Table>
+            
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name} sx={{'&:last-child td, &last-child th':{border:0}}}>
+                  <TableCell align="left" colSpan={4}>
+                    <div className="flex flex-col p-4 rounded-lg space-y2">
+                      <h2 className="underline text-[#b30738] text-xl font-bold">{row.firstName} {row.lastName}</h2>
+                      <br></br>
+                      <p>{row.department}</p>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+        
+         </Table>
+        </TableContainer>
+        </div>
+        </div>
+      
+    </div>
+  )
 }
---
+++ src/components/sidebar/directory-sidebar.jsx
@@ -0,0 +1,113 @@
+"use client"
+
+import { 
+    Sidebar, 
+    SidebarContent, 
+    SidebarHeader, 
+    SidebarGroup 
+} from "@/components/ui/sidebar";
+
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import React, { useState } from "react";
+
+//grouped filters 
+const categories = [
+    {
+        category: "Department", 
+        type: "checkbox",
+        options: [
+            {id: 1, label: "Bioengineering"},
+            {id: 2, label: "Civil, Environmental and Sustainable Engineering"},
+            {id: 3, label: "Computer Science and Engineering"},
+            {id: 4, label: "Electrical and Computer Engineering"},
+            {id: 5, label: "General Engineering"},
+            {id: 6, label: "Mechanical Engineering"},
+        ],
+    },
+    {
+        category: "Has Project Proposal?",
+        type: "radio",
+        options: [
+            {id: 7, label: "Yes"},
+            {id: 8, label: "No"},
+        ],
+    },
+];
+
+export function DirectorySidebar() {
+    /*state initialization: 
+    initialize selectedItems as an empty array which will store the IDs of selection options
+    setSelectedItems updates selectedItems array */
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [radioSelections, setRadioSelections] = useState({
+        "Has Project Proposal?": null,
+    });
+
+    const handleCheckboxChange = (id) => {
+        //if id is unchecked, add it to the array
+        setSelectedItems((prev) =>
+            prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+        );
+    };
+
+    const handleRadioChange = (category, id) => {
+        setRadioSelections((prev) => ({ ...prev, [category]: id }));
+    };
+
+    return (
+        <Sidebar variant="floating" collapsible="none">
+            <SidebarHeader />
+            <SidebarContent>
+                <div className="p-4">
+                    <h2 className="font-semibold text-lg mb-4">Filter</h2>
+                    {categories.map((category) => (
+                        <SidebarGroup key={category.category} className="mb-6 p-1">
+                            <h3 className="font-medium text-sm mb-2">{category.category}</h3>
+                            {category.type === "checkbox" ? (
+                                <ul>
+                                    {category.options.map((option) => (
+                                        <li key={option.id} className="flex items-center mb-2">
+                                            <input
+                                                type="checkbox"
+                                                id={`checkbox-${option.id}`}
+                                                checked={selectedItems.includes(option.id)}
+                                                onChange={() => handleCheckboxChange(option.id)}
+                                                className="mr-2"
+                                                style={{accentColor: "#b30738"}}
+                                            />
+                                            <label htmlFor={`checkbox-${option.id}`} className="text-xs">
+                                                {option.label}
+                                            </label>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <RadioGroup
+                                    value={radioSelections[category.category]}
+                                    onValueChange={(id) => handleRadioChange(category.category, id)}
+                                    className = "flex space-x-4"
+                                >
+                                    {category.options.map((option) => (
+                                        <li key = {option.id} className = "flex items-center mb-2">
+                                            <RadioGroupItem
+                                                id={`radio-${option.id}`}
+                                                value = {option.id}
+                                                checked={radioSelections[category.category] === option.id}
+                                                className="mr-2 ring-2 ring-slate-950"
+                                                style={{transform: "scale(0.5)"}}
+                                            />
+                                        
+                                            <label htmlFor = {`radio-${option.id}`} className = "text-xs">
+                                                {option.label}
+                                            </label>
+                                        </li>
+                                    ))}
+                                </RadioGroup>
+                            )}
+                        </SidebarGroup>
+                    ))}
+                </div>
+            </SidebarContent>
+        </Sidebar>
+    );
+}
