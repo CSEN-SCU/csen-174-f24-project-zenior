7e071c1eb5dad0ba01ef01ff62831ab46beb9842
jcisneros99 <jasoncisneros.jc99@gmail.com>
2024-11-05 17:06:54 -0800

    added working example of pulling proposal list from database

--- src/app/(auth-user)/proposals/page.jsx
+++ src/app/(auth-user)/proposals/page.jsx
@@ -27,6 +27,8 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 
+import { proposals } from "@/lib/server/proposals";
+
 /*
 export default async function Proposals() {
   const allProjects = await projects.get();
@@ -55,62 +57,64 @@ export default async function Proposals() {
 }
 */
 
-function createData(title, description, members, advisor) {
-  return { title, description, members, advisor };
-}
+// function createData(title, description, members, advisor) {
+//   return { title, description, members, advisor };
+// }
 
-/* Will need to pull data from the db */
-const rows = [
-  createData(
-    "AI in Healthcare",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    [
-      {
-        name: "Vani Aggarwal",
-        email: "vani@scu.edu",
-        department: "Computer Science and Engineering",
-      },
-      {
-        name: "Vladimir Ceban",
-        email: "vladimir@scu.edu",
-        department: "Data Science",
-      },
-    ],
-    "Ahmed Amer",
-  ),
-  createData(
-    "Milk Guard",
-    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    [],
-    "Prashanth Asur",
-  ),
-  createData(
-    "Embedded Systems",
-    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    [
-      {
-        name: "Jason Cisneros",
-        email: "jcisneros@scu.edu",
-        department: "Computer Science and Engineering",
-      },
-    ],
-    "None",
-  ),
-  createData(
-    "A Fourth Project",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    [
-      {
-        name: "Mia Lassiter",
-        email: "mlassiter@scu.edu",
-        department: "Web Design and Engineering",
-      },
-    ],
-    "Silvia Figuiera",
-  ),
-];
+// /* Will need to pull data from the db */
+// const rows = [
+//   createData(
+//     "AI in Healthcare",
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//     [
+//       {
+//         name: "Vani Aggarwal",
+//         email: "vani@scu.edu",
+//         department: "Computer Science and Engineering",
+//       },
+//       {
+//         name: "Vladimir Ceban",
+//         email: "vladimir@scu.edu",
+//         department: "Data Science",
+//       },
+//     ],
+//     "Ahmed Amer",
+//   ),
+//   createData(
+//     "Milk Guard",
+//     "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+//     [],
+//     "Prashanth Asur",
+//   ),
+//   createData(
+//     "Embedded Systems",
+//     "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+//     [
+//       {
+//         name: "Jason Cisneros",
+//         email: "jcisneros@scu.edu",
+//         department: "Computer Science and Engineering",
+//       },
+//     ],
+//     "None",
+//   ),
+//   createData(
+//     "A Fourth Project",
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//     [
+//       {
+//         name: "Mia Lassiter",
+//         email: "mlassiter@scu.edu",
+//         department: "Web Design and Engineering",
+//       },
+//     ],
+//     "Silvia Figuiera",
+//   ),
+// ];
 
 export default async function Proposals() {
+  const rows = await proposals(0, 2);
+  console.log(rows);
   return (
     <div className="px-8 m-9">
       <div className="flex flex-row">
@@ -136,7 +140,7 @@ export default async function Proposals() {
                   // this throws a warning because row.name is undefined (hence not a unique key)
                   // not fixing becaue this should be changed to row.id when we have the data in db
                   <TableRow
-                    key={row.name}
+                    key={row.title}
                     sx={{ "&:last-child td, &last-child th": { border: 0 } }}
                   >
                     <TableCell align="left" colSpan={4}>
@@ -165,7 +169,7 @@ export default async function Proposals() {
                         <br></br>
                         <ul className="flex flex-wrap">
                           <li className="font-semibold">Members: </li>
-                          {row.members.map((member, index) => (
+                          {/* {row.members.map((member, index) => (
                             // this key should also be member.id or something like that
                             <li key={index}>
                               <TooltipProvider>
@@ -190,7 +194,7 @@ export default async function Proposals() {
                                 </Tooltip>
                               </TooltipProvider>
                             </li>
-                          ))}
+                          ))} */}
                         </ul>
                         <div>
                           <span className="font-semibold">Advisor</span>:
