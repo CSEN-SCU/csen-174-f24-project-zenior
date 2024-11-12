1036f3665c9aa0a2438f2b9a26b976db0a75a8d4
jcisneros99 <jasoncisneros.jc99@gmail.com>
2024-11-12 15:07:33 -0800

    working faculty retreival function

--- src/lib/server/scholar-commons.js
+++ src/lib/server/scholar-commons.js
@@ -1,21 +1,24 @@
 const baseUrl = 'https://content-out.bepress.com/v2/scholarcommons.scu.edu/';
+const init = {
+  method: 'GET', // or 'POST', 'PUT', etc.
+  headers: {
+      "Authorization" : process.env.DIGITAL_COMMONS_API_TOKEN, // Add any necessary authorization headers
+  }
+};
+
 
 function writeResults(results){
 
-  // console.log(results)
+  console.log(results)
   // Uncomment below to locally write json file of results for debug purposes
-  const fs = require('fs')
-  fs.writeFile("ScholarCommonsResults.json", JSON.stringify(results), (err) => { if(err) throw err;})
+  // const fs = require('fs')
+  // fs.writeFile("ScholarCommonsResults.json", JSON.stringify(results), (err) => { if(err) throw err;})
 }
+
 
 function getApi(endpoint) {
 const endpointUrl = baseUrl + endpoint;
-fetch(endpointUrl, {
-    method: 'GET', // or 'POST', 'PUT', etc.
-    headers: {
-        "Authorization" : process.env.DIGITAL_COMMONS_API_TOKEN, // Add any necessary authorization headers
-    }
-  })
+fetch(endpointUrl, init)
     .then(response => response.json())
     .then(data => writeResults(data))
     .catch(error => console.error('Error:', error));
@@ -26,15 +29,21 @@ fetch(endpointUrl, {
 function getTheses() {
   const queryUrl = baseUrl + "query?virtual_ancestor_link=http://scholarcommons.scu.edu/eng_senior_theses&select_fields=all&limit=1000";
 
-  fetch(queryUrl, {
-    method: 'GET', // or 'POST', 'PUT', etc.
-    headers: {
-        "Authorization" : process.env.DIGITAL_COMMONS_API_TOKEN, // Add any necessary authorization headers
-    }
-  })
+  fetch(queryUrl, init)
     .then(response => response.json())
     .then(data => writeResults(data.results))
     .catch(error => console.error('Error:', error));
 
+
+}
+// postman?
+
+function getFacultyPreviousProjects(faculty, n){
+  const queryUrl = baseUrl + "query?virtual_ancestor_link=http://scholarcommons.scu.edu/eng_senior_theses&select_fields=all" + (n? "&limit=" + n : "") + "&configured_field_t_advisor=" + faculty;
+  console.log(queryUrl);
+  fetch(queryUrl, init)
+    .then(response => response.json())
+    .then(data => writeResults(data.results))
+    .catch(error => console.error('Error:', error));
 
 }
