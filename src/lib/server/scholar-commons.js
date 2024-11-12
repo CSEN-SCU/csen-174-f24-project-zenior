c1dc8e7143f515a53d92b3464555bc5e68b48870
jcisneros99 <jasoncisneros.jc99@gmail.com>
2024-11-12 11:36:29 -0800

    write small function that can take an endpoint arg to go request data from scholar commons

--- src/lib/server/scholar-commons.js
+++ src/lib/server/scholar-commons.js
@@ -1,13 +1,24 @@
 
 
-function getApi() {
-fetch('https://content-out.bepress.com/v2/scholarcommons.scu.edu/eng_senior_theses/fields', {
+function writeResults(results){
+
+  console.log(results)
+  // Uncomment below to locally write json file of results for debug purposes
+  // const fs = require('fs')
+  // fs.writeFile("ScholarCommonsResults.json", JSON.stringify(results), (err) => { if(err) throw err;})
+}
+
+function getApi(endpoint) {
+const baseUrl = 'https://content-out.bepress.com/v2/scholarcommons.scu.edu/';
+const endpointUrl = baseUrl + endpoint;
+fetch(endpointUrl, {
     method: 'GET', // or 'POST', 'PUT', etc.
     headers: {
         "Authorization" : process.env.DIGITAL_COMMONS_API_TOKEN, // Add any necessary authorization headers
     }
   })
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => writeResults(data))
     .catch(error => console.error('Error:', error));
+
 }
