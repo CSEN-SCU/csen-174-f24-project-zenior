ba97786cdee0e51ac55d8dd387f39d4081b49275
vaniaggarwal <52869093+vaniaggarwal@users.noreply.github.com>
2024-12-05 13:52:16 -0800

    Updated Home Pages for Different Views
    - student page: welcome statement, get started, and navigation help
    - added accordion from shadcn
    - faculty page: welcome statement, get started, and navigation help
    - admin page: welcome statement, get started, navigation help
    ------
    for faculty, routing in my team and my profile are not working

--- src/app/(admin)/admin/page.jsx
+++ src/app/(admin)/admin/page.jsx
@@ -1,11 +1,45 @@
 import AuthGuard from "@/components/AuthGuard";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 const AdminPage = () => {
   return (
     <AuthGuard requiredRole="admin">
       <div className="p-6 bg-gray-100 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-4">Admin Dashboard</h1>
-        <p>Manage users, view reports, and access admin tools.</p>
+        <h1 className="text-3xl font-bold mb-4">Admin Portal</h1>
+        <h2 className="text-3x1 font-bold mb-4">Welcome to Zenior!</h2>
+       {/* <p>Manage users, view reports, and access admin tools.</p> */}
+
+        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+
+        <h3 className="text-3x1 font-bold mb-4"> GET STARTED: </h3>
+        <p> 
+            Click on your avatar in the right-hand corner, and head over to <b>My Profile </b>  
+            to fill in your information. 
+        </p>
+
+        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        
+        <h3 className="text-3x1 font-bold mb-4"> Navigating Zenior: </h3>
+        <Accordion type="single" collapsible>
+        <AccordionItem value="item-1"> 
+        <AccordionTrigger className="italic"> Admin Dashboard </AccordionTrigger>
+          <AccordionContent className="pl-5">
+            View accessibility and user data reports and access admin tools. 
+          </AccordionContent>
+        </AccordionItem>  
+        <AccordionItem value="item-2"> 
+        <AccordionTrigger className="italic"> Manage Users </AccordionTrigger>
+          <AccordionContent className="pl-5">
+           Delete, restrict, and edit users. 
+          </AccordionContent>
+        </AccordionItem> 
+        </Accordion>
+
       </div>
     </AuthGuard>
   );
